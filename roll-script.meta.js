d9f79903170a9c2b3fcf6bbb21a50c0f62d24d52
kas-cor <kascorp@gmail.com>
2019-01-06 23:08:50 +0300

    Добавлена иконка и мелкие правки

--- roll-script.meta.js
+++ roll-script.meta.js
@@ -3,11 +3,14 @@
 // @namespace auto-roll-user-js
 // @description Auto roll in freebitco.in
 // @version 190106
+// @author kas-cor
 // @homepageURL https://github.com/kas-cor/roll
 // @supportURL https://github.com/kas-cor/roll/issues
-// @updateURL https://raw.githubusercontent.com/kas-cor/roll/master/roll-script.meta.js
+// @updateURL https://raw.githubusercontent.com/kas-cor/roll/master/roll.meta.js
 // @downloadURL https://raw.githubusercontent.com/kas-cor/roll/master/roll-script.user.js
-// @match https://freebitco.in/
-// @grant none
+// @icon https://raw.githubusercontent.com/kas-cor/roll/master/icon.png
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @match https://freebitco.in/*
+// @grant GM_getValue
 // @run-at document-start
 // ==/UserScript==
--- roll-script.user.js
+++ roll-script.user.js
@@ -3,18 +3,21 @@
 // @namespace auto-roll-user-js
 // @description Auto roll in freebitco.in
 // @version 190106
+// @author kas-cor
 // @homepageURL https://github.com/kas-cor/roll
 // @supportURL https://github.com/kas-cor/roll/issues
 // @updateURL https://raw.githubusercontent.com/kas-cor/roll/master/roll.meta.js
 // @downloadURL https://raw.githubusercontent.com/kas-cor/roll/master/roll-script.user.js
+// @icon https://raw.githubusercontent.com/kas-cor/roll/master/icon.png
 // @require https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
-// @match https://freebitco.in/
-// @grant GM_listValues
+// @match https://freebitco.in/*
 // @grant GM_getValue
 // @run-at document-start
 // ==/UserScript==
 
-$(function () {
+(function () {
+    'use strict';
+
     var timer = {};
     var rewards = [
         {'points': 3200, 'func': 'fp_bonus_1000'},
@@ -24,28 +27,32 @@ $(function () {
         {'points': 32, 'func': 'fp_bonus_10'}
     ];
 
-    function checkReward() {
+    function checkReward(cb) {
+        var reward_points = parseInt($(".user_reward_points").text().replace(',', ""));
         var while_reward = parseInt(GM_getValue('rewards'));
-        var reward_points = parseInt($(".user_reward_points").text());
         var reward_active = $("#reward_points_bonuses_main_div").is(":visible");
         if (while_reward && !reward_active) {
             rewards.forEach(function (v) {
                 if (while_reward === v.points && reward_points >= while_reward) {
                     RedeemRPProduct(v.func);
+                    cb();
                 }
             });
+        } else {
+            cb();
         }
     }
 
     function clickToRoll() {
         var roll = $("#free_play_form_button");
-        checkReward();
         if (roll.is(":visible")) {
-            roll.click();
+            checkReward(function() {
+                roll.trigger('click');
+            });
         }
         window.clearTimeout(timer);
-        timer = window.setTimeout(clickToRoll, 5000 * Math.random() * 5000);
+        timer = window.setTimeout(clickToRoll, 5000 + Math.random() * 5000);
     }
 
     clickToRoll();
-});
+})();
