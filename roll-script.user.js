dd011c4f3b88773a81ad5232633fd09f78e91d1a
kas-cor <kascorp@gmail.com>
2019-11-07 21:52:38 +0300

    fix: Не работает авторолл

--- roll-script.user.js
+++ roll-script.user.js
@@ -2,18 +2,18 @@
 // @name Auto Roll freebitco.in
 // @namespace auto-roll-user-js
 // @description Auto roll in freebitco.in
-// @version 190107_1
+// @version 191107
 // @author kas-cor
 // @homepageURL https://github.com/kas-cor/roll
 // @supportURL https://github.com/kas-cor/roll/issues
 // @updateURL https://raw.githubusercontent.com/kas-cor/roll/master/roll.meta.js
 // @downloadURL https://raw.githubusercontent.com/kas-cor/roll/master/roll-script.user.js
 // @icon https://raw.githubusercontent.com/kas-cor/roll/master/icon.png
-// @require https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @include https://blockchain.info/tobtc*
 // @match https://freebitco.in/*
 // @grant GM_xmlhttpRequest
-// @run-at document-start
+// @run-at document-end
 // ==/UserScript==
 
 (function () {
