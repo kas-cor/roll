d4f8de0fc53c875e6a65d69f0ca185410a2a2b2b
Alexsander <kas-cor@users.noreply.github.com>
2022-06-02 14:11:41 +0300

    Update roll-script.meta.js

--- roll-script.meta.js
+++ roll-script.meta.js
@@ -2,7 +2,7 @@
 // @name Auto Roll freebitco.in
 // @namespace auto-roll-user-js
 // @description Auto roll in freebitco.in
-// @version 280522
+// @version 020622
 // @author kas-cor
 // @homepageURL https://github.com/kas-cor/roll
 // @supportURL https://github.com/kas-cor/roll/issues
