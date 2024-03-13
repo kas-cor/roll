12dbf044882d1714d187ffae3b1788af9fa62eb8
Alexsander <kas-cor@users.noreply.github.com>
2024-03-13 17:11:07 +0300

    Update roll-script.meta.js

--- roll-script.meta.js
+++ roll-script.meta.js
@@ -2,7 +2,7 @@
 // @name Auto Roll freebitco.in
 // @namespace auto-roll-user-js
 // @description Auto roll in freebitco.in
-// @version 121222
+// @version 20241303
 // @author kas-cor
 // @homepageURL https://github.com/kas-cor/roll
 // @supportURL https://github.com/kas-cor/roll/issues
@@ -11,7 +11,7 @@
 // @icon https://raw.githubusercontent.com/kas-cor/roll/master/icon.png
 // @match https://blockchain.info/tobtc*
 // @match https://freebitco.in/*
-// @require https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @require https://cdn.jsdelivr.net/gh/sizzlemctwizzle/GM_config@43fd0fe4de1166f343883511e53546e87840aeaf/gm_config.js
 // @grant GM_getValue
 // @grant GM_setValue
 // @grant GM_registerMenuCommand
